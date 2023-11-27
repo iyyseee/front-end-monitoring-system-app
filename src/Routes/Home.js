e259ff658eee4149336fa4e9785056ec3d6a9225
Julliane Tampus <julliane.tampus123@gmail.com>
2023-11-27 18:12:09 +0800

    added dashboard contents

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -4,7 +4,25 @@ import Dashboard from '../Components/Dashboard'
 function Home() {
   return (
     <Dashboard>
-        <p>hello</p>
+        <button className='col-xl-4 btn bg-gui p-3 border-rounded'>
+          <div className='container'>
+            <h5 className='text-start'>Checking</h5>
+            <div className='d-flex flex-row align-items-center'>
+              <img className='icon-primary' src='/imgs/bus.png' alt=''/>
+              <p><span className='fc-primary-logo'>CB-Transco</span> Hino Passenger Checking and Fare Collecting.</p>
+            </div>
+          </div>
+        </button>
+
+        <button className='col-xl-4 btn bg-gui p-3 border-rounded'>
+          <div className='container'>
+            <h5 className='text-start'>Transaction</h5>
+            <div className='d-flex flex-row align-items-center'>
+              <img className='icon-primary' src='/imgs/receipt.png' alt=''/>
+              <p><span className='fc-primary-logo'>CB-Transco</span> Hino Transaction Records.</p>
+            </div>
+          </div>
+        </button>
     </Dashboard>
   )
 }
