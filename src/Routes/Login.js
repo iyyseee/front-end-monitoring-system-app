7299f0d5ce783c98d5bbf09ed1f9a6aaa91d5a6b
Julliane Tampus <julliane.tampus123@gmail.com>
2023-11-27 20:44:11 +0800

    update login page

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import { useState , useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 function Login() {
+
+    const [isEmail, setisEmail] = useState(false);
+
+    const [isPassword, setisPassword] = useState(false);
+
+    const [showPass, setshowPass] = useState(false);
+
+    const [inputs, setinputs] = useState({
+        email : '',
+        password : '',
+    });
+
+
+    const handdlesubmit = ()=>{
+        if(inputs.email.length == 0) return setisEmail(true);
+        if(inputs.password.length == 0) return setisPassword(true)
+    }
+
+    useEffect(() => {
+        setisEmail(false)
+        setisPassword(false)
+    }, [inputs]);
+
+
   return (
     <div> 
       <div className='d-flex flex-column align-items-center justify-content-center  vh-100'>
@@ -12,9 +38,14 @@ function Login() {
               </div>
           </div>
           <p>Login your dispatcher account</p>
-          <input className='input-primary' type='email' placeholder='email' />
-          <input className='input-primary' type='password' placeholder='password' />
-          <button className='btn-primary'>Login</button>
+          <input className={isEmail ? 'input-primary error' : 'input-primary'} type='email' placeholder='email' value={inputs.email} onChange={e=>setinputs({...inputs , email : e.target.value})} />
+          <input className={isPassword ? 'input-primary error' : 'input-primary'} type={showPass ? 'text' : 'password'} placeholder='password' value={inputs.password} onChange={e=>setinputs({...inputs , password : e.target.value})} />
+          <div className='d-flex flex-row gap-2'>
+            <input onClick={() => showPass ? setshowPass(false) : setshowPass(true)} type='checkbox' id='showpass' />
+            <label htmlFor='showpass'>Show password</label>
+          </div>
+          <button onClick={handdlesubmit} className='btn-primary'>Login</button>
+          <p><Link to={'/admin/login'}>Click here</Link> to login as Admin.</p>
       </div>
     </div>
   )
