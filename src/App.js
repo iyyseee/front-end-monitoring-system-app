666e1d217f039fe113ee52a225b7cb3740dfc5bb
Julliane Tampus <julliane.tampus123@gmail.com>
2023-11-27 15:40:09 +0800

    added Login and dasbard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home';
+import Login from './Routes/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/> 
+        <Route path='/login' element={<Login/>}/> 
       </Routes>
     </Router>
   );
