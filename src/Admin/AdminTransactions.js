4f114209610bfe63b5f682fedfc0a864654a3ce9
Julliane Tampus <julliane.tampus123@gmail.com>
2023-12-11 02:31:18 +0800

    added admin transaction functionality

---
+++ src/Admin/AdminTransactions.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import AdminNav from './Components/AdminNav'
+import { useState , useEffect } from 'react'
+import Loading from '../Components/Loading';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+function AdminTransactions() {
+
+    const [isLoading, setisLoading] = useState(false);
+    const [transactions, settransactions] = useState([]);
+
+    useEffect(() => {
+        setisLoading(true)
+        axios.get(process.env.REACT_APP_API_URL + '/transactions/all', {headers : {'Authorization' : 'Bearer ' + Cookies.get('token')}}).then(e=>{
+          console.log(e.data)
+          settransactions(e.data)
+          setisLoading(false)
+        })
+        
+      }, []);
+
+    return (
+        <AdminNav>
+            {!isLoading ? 
+          <div className='col-xl-4 col-12'>
+          {transactions.map((transaction , index)=>{
+            return (
+              <a href={`/admin/transactions/${transaction.id}`} key={index} className='bg-gui p-3 rounded d-flex flex-row justify-content-between align-items-center no-link mt-2' >
+                <div className='d-flex flex-row'>
+                  <img className='icon-small' src='/imgs/transaction.png' alt='transaction'/>
+                  <p className='text-small'>
+                    {transaction.name}<br/>
+                    Origin: {transaction.origin}<br/>  
+                    Dispatcher {transaction.full_name}   
+                </p>
+                </div>
+                <p className='text-end text-small'>{transaction.date_time}</p>
+              </a>
+            )
+          })}
+          </div>
+        : <Loading/>}
+        { !isLoading ? (transactions.length == 0) ? <p>No Transactions Available.</p>: <></> : <></>}
+        </AdminNav>
+    )
+}
+
+export default AdminTransactions
--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import AdminDispatcher from './Admin/AdminDispatcher';
 import AdminDispatcherByID from './Admin/AdminDispatcherByID';
 import AdminBus from './Admin/AdminBus';
 import AdminBusByID from './Admin/AdminBusByID';
+import AdminTransactions from './Admin/AdminTransactions';
+import AdminTransactionByID from './Admin/AdminTransactionByID';
 
 function App() {
   return (
@@ -44,6 +46,8 @@ function App() {
         <Route path='/admin/dispatchers/:id' element={<AdminDispatcherByID/>} />
         <Route path='/admin/hino' element={<AdminBus/>} />
         <Route path='/admin/hino/:id' element={<AdminBusByID/>} />
+        <Route path='/admin/transactions' element={<AdminTransactions/>} />
+        <Route path='/admin/transactions/:id' element={<AdminTransactionByID/>} />
       </Routes>
     </Router>
   );
